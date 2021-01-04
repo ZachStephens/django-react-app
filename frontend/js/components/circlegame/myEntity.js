a5e0e4427725cab63b5974dd35c2000b3beae86e
Zach <stephens2633@gmail.com>
2021-01-03 19:44:01 -0500

    explosions after mass reach threshold

--- frontend/js/components/circlegame/myEntity.js
+++ frontend/js/components/circlegame/myEntity.js
@@ -1,8 +1,4 @@
 
-
-import singleGrape from '../../../res/single-grape.jpg'
-
-
 function massToRadius(mass)
 {
     return  Math.floor(Math.pow(mass,1/2))
@@ -47,7 +43,7 @@ class MyEntity  {
         return {unit,mag}
     }
 
-    applyForce(dTo,otherMass,k=.028,rFallOff=1.4){
+    applyForce(dTo,otherMass,k=.78,rFallOff=1.7){
         if(this.fixed){
             return
         }
@@ -68,6 +64,7 @@ class MyEntity  {
         largerEntity.r = massToRadius(largerEntity.mass)
         smallerEntity.enable = false
         smallerEntity.drawEnable = false
+        console.log("absorbed mass now: "+ largerEntity.mass)
     }
 
     draw(ctx,win){
@@ -90,4 +87,110 @@ class MyEntity  {
     }
   }
 
-  export default MyEntity;
+
+class EntityManager  {
+    constructor(window) {
+        this.entityList = new Array();
+        this.entityStagingList = new Array();
+        this.entityDeletingList = new Array();
+        var floatingSpeed = 20
+         for(let i=0;i<600;i++){
+             let pos={xpos: window.maxWidth*(1/2 + 5*(Math.random() - .5)), ypos: window.maxHeight*(1/2 + 5*(Math.random() - .5))}
+             let vel = {xvel: floatingSpeed*(Math.random()-.5), yvel: floatingSpeed*(Math.random()-.5)}
+             // let vel = {xvel: 0, yvel: 0}
+             let newEntity = new MyEntity(pos,vel,1200)
+             this.entityList.push(newEntity)
+         } 
+         // {
+         // let pos={xpos: window.maxWidth*.5, ypos: window.maxHeight*.5}
+         // let vel = {xvel: 0  , yvel: 0}
+         // let newEntity = new MyEntity(pos,vel,1000000,'hsl(' + 5 + ', 100%, 60%)')
+         // this.entityList.push(newEntity) 
+         // }
+    }
+
+    update(ctx, frameCount, window){
+
+
+        // this.entityDeletingList.forEach((i) => {this.entityList.splice(i, 1)});
+        // if(this.entityStagingList.length > 0)
+        // {
+        //     for (var i = this.entityStagingList.length - 1; i > -1; i--){
+        //     this.entityList.push(this.entityStagingList[0])
+        //     }
+        //     this.entityStagingList = []
+        // }
+        // this.entityStagingList = []
+        // this.entityDeletingList  = []
+        
+
+        const maxWidth = window.maxWidth*20
+        const minWidth = -window.maxWidth*20
+        const maxHeight = window.maxHeight*20
+        const minHeight = -window.maxHeight*20
+        const threshholdMass = 100000
+
+
+        for (var i = this.entityList.length - 1; i > -1; i--) {
+            
+            let entity = this.entityList[i]
+
+            //console.log(entity)
+            let newX = entity.xpos + entity.xvel
+            let newY = entity.ypos + entity.yvel
+
+            if(newX > maxWidth || newX < minWidth || newY > maxHeight || newY < minHeight || !entity.enable){
+                // this.entityDeletingList.push(i)
+                console.log('deleting: '+i)
+                this.entityList.splice(i, 1)
+                continue
+            }
+
+            this.entityList.forEach((otherEntity)=>{
+                if(otherEntity != entity){
+                    let dTo = entity.distanceTo(otherEntity)
+                    if(dTo.mag < (entity.r + otherEntity.r))
+                    {
+                        entity.absorb(otherEntity)
+                    }
+                    entity.applyForce(dTo, otherEntity.mass)
+                }
+            });
+
+            if(entity.mass > threshholdMass)
+            {
+                console.log("Over Threshold")
+                const subEntityQuantity = 20
+                const subEntityMass = Math.floor(entity.mass/subEntityQuantity)
+                console.log(subEntityMass)
+                const subEntityAngle = 2*Math.PI / subEntityQuantity
+                const initRadius = 1000
+                for (var i = 0; i < subEntityQuantity; i++)
+                {
+                    // console.log(entity.xpos)
+                    // console.log(entity.ypos)
+                    let subEntityVecX = Math.cos(subEntityAngle* i) * initRadius
+                    //console.log(subEntityVecX)
+                    let subEntityVecY = Math.sin(subEntityAngle* i) * initRadius
+                    // console.log(subEntityVecY)
+                    let pos={xpos: entity.xpos + subEntityVecX, ypos: entity.ypos + subEntityVecY}
+                    console.log(pos)
+                    let vel = {xvel: 100*(subEntityVecX/initRadius), yvel: 100*(subEntityVecY/initRadius)}
+                    //console.log(vel)
+                    let newEntity = new MyEntity(pos,vel,subEntityMass)
+                    this.entityList.push(newEntity)
+                    // this.entityStagingList.push(newEntity)
+                }
+                entity.enable = false
+                this.entityList.splice(i, 1)
+                
+            }
+
+            entity.updatePos(newX,newY)
+            entity.draw(ctx,window)
+        }
+  }
+}
+
+
+export default EntityManager;
